acb34d7079abab9c5974ea27818e0a042baf8074
sbb2005 <sbb2005@naver.com>
2021-08-10 02:39:48 +0000

    2nd commit

--- data/csv_make.py
+++ data/csv_make.py
@@ -1,9 +1,11 @@
 import csv
 import os
 import json
+import pickle
 
 filelist = os.listdir('data/overall')
 
+# csv file
 for i in range(len(filelist)):
     filename = os.path.splitext(filelist[i])
     filename = filename[0]
@@ -20,3 +22,12 @@ for i in range(len(filelist)):
     print(f"{filename}.csv was writen successfully.")
 
 print("Making .csv is finished!!")
+
+# readme.md
+with open('data/csv/readme.md', 'w') as f:
+    info = match['info']['participants'][0]
+    infoTypes =  [type(v) for v in info.values()]
+    infoKeys = [k for k in info.keys()]
+    for num in range(len(info)):
+        line = str(num) + "\t" + str(infoTypes[num]) + "\t" + str(infoKeys[num]) + "\n"
+        f.write(line)
